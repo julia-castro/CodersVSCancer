fad641026cca66c855c315e4aa2db4519cac3ec9
julia-castro <julia.castro@outlook.com>
2015-09-11 23:57:00 -0400

    add constraints to quest migration

--- db/migrate/20150912033149_create_quests.rb
+++ db/migrate/20150912033149_create_quests.rb
@@ -1,8 +1,8 @@
 class CreateQuests < ActiveRecord::Migration
   def change
     create_table :quests do |t|
-      t.string :name
-      t.references :user
+      t.string :name, null: false
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
