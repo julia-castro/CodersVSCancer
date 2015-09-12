512daa7e508e010cce065364dd3f567537e7bdea
julia-castro <julia.castro@outlook.com>
2015-09-12 00:14:44 -0400

    add association between user and friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   has_many :quests
   has_many :nudges
   belongs_to :demographic
+  has_many :friendships
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 end
+
+
+has_many :patients, through: :appointments
