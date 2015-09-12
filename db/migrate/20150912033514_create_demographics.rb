0b4d3e98ff70dfedc57b23fd26ebd6bca88b8ccf
julia-castro <julia.castro@outlook.com>
2015-09-11 23:58:47 -0400

    add constraints to demographics migration

--- db/migrate/20150912033514_create_demographics.rb
+++ db/migrate/20150912033514_create_demographics.rb
@@ -1,9 +1,9 @@
 class CreateDemographics < ActiveRecord::Migration
   def change
     create_table :demographics do |t|
-      t.string :gender
-      t.string :age
-      t.references :user
+      t.string :gender, null: false
+      t.string :age, null: false
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
